22d2ec887a821f1bd3303f5a17ee2f47c5224c2b
romaunte <182824818+romaunte@users.noreply.github.com.>
2025-12-24 13:35:51 +0300

    Working version

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set "MINGW_PATH=E:\ProgramFiles\C++\mingw64\bin"
+set "PATH=%MINGW_PATH%;%PATH%"
+
+set BUILD_TYPE=Ninja
+set BUILD_SUFFIX=ninja
+
+chcp 65001
+
+set BUILD_FOLDER=build_%BUILD_SUFFIX%
+set SOURCE_FOLDER=src
+
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+
+cd %BUILD_FOLDER%
+
+cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
+cmake --build .
+
+cd ..
